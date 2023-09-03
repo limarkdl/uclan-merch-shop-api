f4534cc0ddf3200ccac5258912c6cfb374d7d4cc
limarkdl <ikostin@uclan.ac.uk>
2023-09-03 06:31:32 +0300

    Versions conflict fix

--- productsRouter.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-const productsRouter = new Router();
-import productsController from './productsController.js';
-
-productsRouter.get('/all', productsController.getAllProducts);
-productsRouter.get('/:id', productsController.getProductByID);
-
-export default productsRouter;
